ed3de38b52a58d7bdb149c056d0027c3a4355172
fewzy <bj@Benjamins-iMac.local>
2013-11-02 18:59:36 +0000

    TASK 2 SET

--- lib/MetiClock/clitools.rb
+++ lib/MetiClock/clitools.rb
@@ -17,5 +17,14 @@ module CliTools
 end
 
 # TASK: Write a method here that does the same as 'put_each', but also makes two new lines 
-# afterwards. Call it put_each_nl.
+# afterwards. Call it self.put_each_nl
 
+# TASK 2: Write a method here, called gen_logo, that uses 'put_each' to print the following text:
+=begin
+ __    __     ______     ______   __     ______     __         ______     ______     __  __    
+/\ "-./  \   /\  ___\   /\__  _\ /\ \   /\  ___\   /\ \       /\  __ \   /\  ___\   /\ \/ /    
+\ \ \-./\ \  \ \  __\   \/_/\ \/ \ \ \  \ \ \____  \ \ \____  \ \ \/\ \  \ \ \____  \ \  _"-.  
+ \ \_\ \ \_\  \ \_____\    \ \_\  \ \_\  \ \_____\  \ \_____\  \ \_____\  \ \_____\  \ \_\ \_\ 
+  \/_/  \/_/   \/_____/     \/_/   \/_/   \/_____/   \/_____/   \/_____/   \/_____/   \/_/\/_/ 
+
+=end # Ignore the begin and end.
